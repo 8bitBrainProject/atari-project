72cd2adc1e8f92813cee42e4ce2ddc52cea3406f
shannonleemcd <shannon.lee.mcd@outlook.com>
2020-10-25 14:47:01 -0500

    Create main.py
    Initial Commit of main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+COMP 6000 FINAL PROJECT
+
+TEAM: 8-BIT BRAIN
+
+@author: Dennis Brown (dgb0028@auburn.edu)
+@author: Shannon McDade (slm0035@auburn.edu)
+@author: Jacob Parmer (jdp0061@auburn.edu)
+
+DESCRIPTION: main - run everything from here
+
+RESOURCES:
+
+"""
+from agents.DqnAgent import DqnAgent
+
+def main(msg='replace_me'):
+    return print(msg)
+
+if __name__ == "__main__":
+    main()
