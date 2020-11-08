1f1493b294bcff3f0364c6cb6c6bfad5fcc56c52
Jacob Parmer <jdp0061@auburn.edu>
2020-11-08 15:36:57 -0500

    Program now runs for number of episodes and chooses action based on state (kinda)

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ import torch
 import gym
 from agents.DqnAgent import DqnAgent
 
+NUMBER_EPISODES = 200
+
 def main():
     """
     main
@@ -26,28 +28,39 @@ def main():
     None.
 
     """
-    env = gym.make('BreakoutDeterministic-v4')
-    frame = env.reset()
 
+    env = gym.make('SpaceInvadersNoFrameskip-v4')
 
-    state_size = 8 
-    print(state_size)
+    state_size = 4 
     action_size = env.action_space.n
-    print(action_size)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     agent = DqnAgent(state_size=state_size, action_size=action_size, device=device, seed=0)
 
+    for i in range(NUMBER_EPISODES):
 
-    env.render()
+        frame = env.reset()
+        image_data = agent.image_preprocessing(frame)
 
-    finished = False
-    while not finished:
-        
-        action = env.action_space.sample()
-        frame, reward, finished, _ = env.step(action)
+        state = torch.cat((image_data, image_data, image_data, image_data)).unsqueeze(0)
+
         env.render()
 
+        finished = False
+        
+        while not finished:
+        
+            action = agent.choose_action(state)
+            final_epsilon_reached = agent.iterate_epsilon()
+
+            frame, reward, finished, _ = env.step(action)
+
+            frame = agent.image_preprocessing(frame)
+            next_state = torch.cat((state.squeeze(0)[3:, :, :], frame)).unsqueeze(0)
+
+            state = next_state
+
+            env.render()
 
 if __name__ == "__main__":
     main()
