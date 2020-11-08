8e76bf2271eda0cd8f679d7f9a255e719a15cc7a
Jacob Parmer <jdp0061@auburn.edu>
2020-11-08 15:37:15 -0500

    QNetwork now follows flappy bird example rather than medium example

--- models/QNetwork.py
+++ models/QNetwork.py
@@ -50,13 +50,23 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
 
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1)
-        self.fc2 = nn.Linear(fc1, fc2)
-        self.fc3 = nn.Linear(fc2, action_size)
+        #self.fc1 = nn.Linear(state_size, fc1)
+        #self.fc2 = nn.Linear(fc1, fc2)
+        #self.fc3 = nn.Linear(fc2, action_size)
+
+        self.conv1 = nn.Conv2d(in_channels=12, out_channels=32, kernel_size=16, stride=8)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=8, stride=4)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.fc4 = nn.Linear(128, 1)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.fc5 = nn.Linear(1, action_size)
+
 
     def forward(self, x):
         """
-
 
         Parameters
         ----------
@@ -69,7 +79,19 @@ class QNetwork(nn.Module):
             DESCRIPTION.
 
         """
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        #x = F.relu(self.fc1(x))
+        #x = F.relu(self.fc2(x))
 
-        return self.fc3(x)
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.conv3(out)
+        out = self.relu3(out)
+        out = out.view(out.size()[0], -1)
+        out = self.fc4(out)
+        out = self.relu4(out)
+        out = self.fc5(out)
+
+        return out
+        #return self.fc3(x)
