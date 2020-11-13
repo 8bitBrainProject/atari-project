6425bbf23b4738f2ae3c5bf15880eb3172bdb5f1
shannonleemcd <shannon.lee.mcd@outlook.com>
2020-11-13 15:36:40 -0600

    added to ignore rewardlog.txt, added PpoAgent, added ppo_settings
    added to ignore rewardlog.txt, added PpoAgent, added ppo_settings

---
+++ config/ppo_settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+COMP 6000 FINAL PROJECT
+
+TEAM: 8-BIT BRAIN
+
+@author: Dennis Brown (dgb0028@auburn.edu)
+@author: Shannon McDade (slm0035@auburn.edu)
+@author: Jacob Parmer (jdp0061@auburn.edu)
+
+DESCRIPTION: PPO Settings
+
+RESOURCES:
+
+"""
+
+# eps clip
+EPISODE_CLIP = 0.1
+
+# Discount Factor (Gamma)
+GAMMA = 0.99
+
+# Learning rate
+LEARNING_RATE = 1e-3
